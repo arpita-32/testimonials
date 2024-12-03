960152233f1902a3628401ec7a8849fc44f781a3
arpita-32 <arpitasundaray9@gmail.com>
2024-12-03 09:22:39 +0530

    first commit

---
+++ src/Components/dist/Testimonial.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Testimonial = function Testimonial() {};
+//# sourceMappingURL=Testimonial.dev.js.map
